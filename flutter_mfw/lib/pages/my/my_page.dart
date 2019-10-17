1ac346679ff1ccb260f0bc6ea312bac6caf14082
董家祎 <575801327@qq.com>
2019-10-17 22:14:56 +0800

    我的页面布局

--- flutter_mfw/lib/pages/my/my_page.dart
+++ flutter_mfw/lib/pages/my/my_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mfw/pages/my/widget/my_navbar_widget.dart';
+import 'package:flutter_mfw/pages/my/widget/my_logout_widget.dart';
+import 'package:flutter_mfw/pages/my/widget/my_items_widget.dart';
 class MyPage extends StatefulWidget {
   @override
   _MyPageState createState() => _MyPageState();
@@ -7,6 +10,18 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(242, 242, 242, 1.0),
+
+      body: Column(
+        children: <Widget>[
+          MyNavbarWidget(),
+          MyLogoutWidget(),
+          MyItemsWidget()
+        ],
+      ),
+    );
   }
+
+
 }
---
+++ flutter_mfw/lib/pages/my/widget/my_navbar_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mfw/screen_adapter.dart';
+class MyNavbarWidget extends StatefulWidget {
+  @override
+  _MyNavbarWidgetState createState() => _MyNavbarWidgetState();
+}
+
+class _MyNavbarWidgetState extends State<MyNavbarWidget> {
+  @override
+  Widget build(BuildContext context) {
+    ScreenAdapter.init(context);
+    return Container(
+     // alignment: Alignment.bottomCenter,
+      padding: EdgeInsets.only(bottom: 10),
+      width: double.infinity,
+      height: ScreenAdapter.getStatusBarHeight()+ScreenAdapter.setHeight(88),
+      color: Color.fromRGBO(254, 217, 49, 1.0),
+      child: Stack(
+        children: <Widget>[
+
+          Positioned(
+            bottom: 5,
+            left: 20,
+            child: _leadingsWidget(),
+          ),
+          Positioned(
+            right: 20,
+            bottom: 5,
+            child: Image.asset("assets/images/icon_message.png",width:20,height: 20),
+          )
+
+        ],
+      ),
+    );
+  }
+  Widget _leadingsWidget(){
+
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+          child:  Image.asset("assets/images/setting.png"),
+        ),
+        Image.asset("assets/images/saoma.png")
+      ],
+    );
+  }
+
+}
