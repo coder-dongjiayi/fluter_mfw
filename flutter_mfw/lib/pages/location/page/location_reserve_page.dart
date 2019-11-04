6f62d847fd773b8415e13fcfbae28e657c33e6d3
董家祎 <575801327@qq.com>
2019-11-04 21:57:04 +0800

    no message

--- flutter_mfw/lib/pages/location/page/location_reserve_page.dart
+++ flutter_mfw/lib/pages/location/page/location_reserve_page.dart
@@ -7,10 +7,42 @@ class LocationReservePage extends StatefulWidget {
 class _LocationReservePageState extends State<LocationReservePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("预定"),
-      ),
+    return Column(
+      children: <Widget>[
+        _liveWhere()
+      ],
     );
   }
+
+  Widget _liveWhere(){
+    return Column(
+
+      children: <Widget>[
+        _titleWhere()
+      ],
+    );
+  }
+  Widget _titleWhere(){
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text("住哪里",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w600),),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text("查看更多",style: TextStyle(fontWeight: FontWeight.w200),),
+              Icon(Icons.keyboard_arrow_right)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
 }
