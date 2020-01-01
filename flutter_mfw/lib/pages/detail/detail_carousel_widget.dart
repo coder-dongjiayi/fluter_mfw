3d6bf5cd852b729de3290da5066813bd20014a99
董家祎 <575801327@qq.com>
2020-01-01 20:25:51 +0800

    no message

--- flutter_mfw/lib/pages/detail/detail_carousel_widget.dart
+++ flutter_mfw/lib/pages/detail/detail_carousel_widget.dart
@@ -23,30 +23,58 @@ class _DetailCarouselWidgetState extends State<DetailCarouselWidget> {
   @override
   Widget build(BuildContext context) {
 
+    ScreenAdapter.init(context);
+
+    return Stack(
+      children: <Widget>[
+        _carousel(),
+        Positioned(
+          right: 15,
+          bottom: 15,
+          child: _control(),
+
+        )
+      ],
+    );
+  }
+
+  Widget _control(){
+   return Container(
+     padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
+     alignment: Alignment.center,
+     decoration: BoxDecoration(
+       color: Color.fromRGBO(0, 0, 0, 0.5),
+       borderRadius: BorderRadius.circular(20)
+
+     ),
+     child: Text("${_index+1}" + "/" + "${widget.medias.length}",style:TextStyle(color: Colors.white,fontSize: 10),),
+   );
+  }
+  Widget _carousel(){
+
     double image_width = widget.medias[_index].width.toDouble();
     double image_height = widget.medias[_index].height.toDouble();
 
     double rate = image_width / image_height;
     double real_height = ScreenAdapter.getScreenWidth() / rate;
 
-    ScreenAdapter.init(context);
-    return Container(
+   return Container(
 
-      height:ScreenAdapter.setHeight(real_height),
-      width: double.infinity,
-      child: PageView(
-        controller: _controller,
-        onPageChanged: (index){
-          setState(() {
-            _index = index;
+     height:ScreenAdapter.setHeight(real_height),
+     width: double.infinity,
+     child: PageView(
+       controller: _controller,
+       onPageChanged: (index){
+         setState(() {
+           _index = index;
 
-          });
-        },
-        children: widget.medias.map((value){
-          return Image.network(value.bimg);
-        }).toList(),
-      ),
-    );
+         });
+       },
+       children: widget.medias.map((value){
+         return Image.network(value.bimg);
+       }).toList(),
+     ),
+   );
   }
 
 }
